a01611b0062d3e89645eeb5f68152988f4b06f42
mohamedali10 <bouine.mohamedali@gmail.com>
2017-08-30 13:46:41 +0100

    last commit

--- src/pages/cadence/cadence.ts
+++ src/pages/cadence/cadence.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { NavController, NavParams, Events } from 'ionic-angular';
 import { BLE } from '@ionic-native/ble';
 @Component({
@@ -7,141 +7,144 @@ import { BLE } from '@ionic-native/ble';
 
 })
 
-export class CadencePage {
+export class CadencePage implements OnDestroy {
+
+
   cadns_val: any = 0;
-  show_cd: boolean = false;
   chart: any;
-  options: any;
+  options: any = {
+    chart: {
+      type: 'gauge',
+      plotBackgroundColor: null,
+      plotBackgroundImage: null,
+      plotBorderWidth: 0,
+      plotShadow: false
+    },
+    credits: {
+      enabled: false
+    },
 
-  constructor(public navCtrl: NavController, private event: Events, public navParams: NavParams, public ble: BLE) {
-    this.options = {
-      chart: {
-        type: 'gauge',
-        plotBackgroundColor: null,
-        plotBackgroundImage: null,
-        plotBorderWidth: 0,
-        plotShadow: false
-      },
+    title: {
+      text: 'Speedometer'
+    },
 
-      title: {
-        text: 'Speedometer'
-      },
-
-      pane: {
-        startAngle: -150,
-        endAngle: 150,
-        background: [{
-          backgroundColor: {
-            linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 },
-            stops: [
-              [0, '#FFF'],
-              [1, '#333']
-            ]
-          },
-          borderWidth: 0,
-          outerRadius: '109%'
-        }, {
-          backgroundColor: {
-            linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 },
-            stops: [
-              [0, '#333'],
-              [1, '#FFF']
-            ]
-          },
-          borderWidth: 1,
-          outerRadius: '107%'
-        }, {
-          // default background
-        }, {
-          backgroundColor: '#DDD',
-          borderWidth: 0,
-          outerRadius: '105%',
-          innerRadius: '103%'
-        }]
-      },
-
-      // the value axis
-      yAxis: {
-        min: 0,
-        max: 200,
-
-        minorTickInterval: 'auto',
-        minorTickWidth: 1,
-        minorTickLength: 10,
-        minorTickPosition: 'inside',
-        minorTickColor: '#666',
-
-        tickPixelInterval: 30,
-        tickWidth: 2,
-        tickPosition: 'inside',
-        tickLength: 10,
-        tickColor: '#666',
-        labels: {
-          step: 2,
-          rotation: 'auto'
+    pane: {
+      startAngle: -150,
+      endAngle: 150,
+      background: [{
+        backgroundColor: {
+          linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 },
+          stops: [
+            [0, '#FFF'],
+            [1, '#333']
+          ]
         },
-        title: {
-          text: 'km/h'
+        borderWidth: 0,
+        outerRadius: '109%'
+      }, {
+        backgroundColor: {
+          linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 },
+          stops: [
+            [0, '#333'],
+            [1, '#FFF']
+          ]
         },
-        plotBands: [{
-          from: 0,
-          to: 120,
-          color: '#55BF3B' // green
-        }, {
-          from: 120,
-          to: 160,
-          color: '#DDDF0D' // yellow
-        }, {
-          from: 160,
-          to: 200,
-          color: '#DF5353' // red
-        }]
-      },
-
-      series: [{
-        name: 'Speed',
-        data: [0],
-        tooltip: {
-          valueSuffix: ' km/h'
-        }
+        borderWidth: 1,
+        outerRadius: '107%'
+      }, {
+        // default background
+      }, {
+        backgroundColor: '#DDD',
+        borderWidth: 0,
+        outerRadius: '105%',
+        innerRadius: '103%'
       }]
-    };
-    /*after setting gauge option subscribe to cadence notif*/
-    console.log('gauge setted');
-    this.event.subscribe('gaugeVal', (data) => { this.UpdateGauge(data); });
+    },
+
+    // the value axis
+    yAxis: {
+      min: 0,
+      max: 200,
+
+      minorTickInterval: 'auto',
+      minorTickWidth: 1,
+      minorTickLength: 10,
+      minorTickPosition: 'inside',
+      minorTickColor: '#666',
+
+      tickPixelInterval: 30,
+      tickWidth: 2,
+      tickPosition: 'inside',
+      tickLength: 10,
+      tickColor: '#666',
+      labels: {
+        step: 2,
+        rotation: 'auto'
+      },
+      plotBands: [{
+        from: 0,
+        to: 120,
+        color: '#55BF3B' // green
+      }, {
+        from: 120,
+        to: 160,
+        color: '#DDDF0D' // yellow
+      }, {
+        from: 160,
+        to: 200,
+        color: '#DF5353' // red
+      }]
+    },
+
+    series: [{
+      name: 'Speed',
+      data: [0],
+    }]
+  };
+  subscription: any;
+  constructor(public navCtrl: NavController, public navParams: NavParams, public ble: BLE) { }
+
+  ionViewDidEnter() {
+    this.CadenceVal();
   }
 
 
-
-
-  ionViewDidEnter() {
-    console.log('ionViewDidEnter CadencePage');
-    this.CadenceVal();
+  saveInstance(chartInstance) {
+    //for chart appearence
+    this.chart = chartInstance;
   }
 
   CadenceVal() {
     console.log('start getting cads values');
-    this.ble.startNotification("F5:56:71:04:1B:60"
+    this.subscription = this.ble.startNotification("F5:56:71:04:1B:60"
       , "99ddcda5-a80c-4f94-be5d-c66b9fba40cf", "99dd0305-a80c-4f94-be5d-c66b9fba40cf")
       .subscribe(cadence => {
         this.cadns_val = this.onData(cadence);
-        console.log('cadence value' + this.cadns_val);
-        this.event.publish('gaugeVal', this.cadns_val);
+        if (this.cadns_val[0] != 0) {
+          setTimeout(() => { this.UpdateGauge(this.cadns_val[0]) }, 750);
+        }
+        else {
+          this.UpdateGauge(0);
+        }
+        console.log('cadence value' + this.cadns_val[0]);
       },
       error => { console.log('thisispb:' + error); });
   }
-  //action to execute each time a cadence val captured
+
   UpdateGauge(data: any) {
     this.chart.series[0].setData([]);
-    console.log('UpdateGauge' + data);
     this.chart.series[0].setData([data]);
   }
-  saveInstance(chartInstance): void {
-    this.chart = chartInstance;
-  }
+
+
+
   onData(buffer) {
     var data = new Uint8Array(buffer);
     return data;
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
 
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { NavController, Platform } from 'ionic-angular';
+import { Component, NgZone } from '@angular/core';
+import { NavController, Platform, LoadingController, ToastController         } from 'ionic-angular';
 import { BLE } from '@ionic-native/ble';
 import { CadencePage } from '../cadence/cadence';
 @Component({
@@ -21,49 +21,46 @@ export class HomePage {
   cadns_service: string;
   cadns_char: string;
 
-  /*----Characteristec Values -----*/
-  revolution_nb: any;
+  /*----Characteristecs Value -----*/
+  revolution_nb: any = 0;
   Bt_lev: number;
-
-
-  /*-----Intermediate vars------*/
-  connection: boolean = false;
+  batteryLevel: any;
   last_revolution_nb: any = 0;
-
+  /*-----Intermediate vars : Boolean vars are for displaying HTML elements------*/
+  isRev: boolean = false;
+  isValid: boolean = false;
+  RevoSubscription: any;
   /*------------- Methods ------------*/
-  constructor(public navCtrl: NavController, private platform: Platform, public ble: BLE) {
-    platform.ready().then(() => {
-      console.log('Android ready to use');
-    });
-
-    if (ble.isEnabled()) {
-      console.log('step one cool');
-
-    }
-    else {
-      ble.enable();
-      console.log('ble activate.');
-    }
-    console.log('ble!ok');
-
-    this.scan();
+  constructor(private ngZone: NgZone,private toastCtrl: ToastController, public load: LoadingController, public navCtrl: NavController, private platform: Platform, public ble: BLE) {
+    this.ble.isEnabled().then(() => {
+      console.log(' bl ok start scanning now');
+      this.scan();
+    },
+      () => {
+        console.log(' bl not ok enable it and start scanning');
+        this.ble.enable().then(() => {
+          console.log('go go');
+          this.scan();
+        });
+      });
 
   }
-
   scan() {
     this.ble.startScan([]).subscribe(devices => {
       if (devices.id == 'F5:56:71:04:1B:60') {
         this.ble.stopScan().then
           (() => {
-            console.log('DEVICE FOUND' + devices.name);
+            console.log('DEVICE FOUND: ' + devices.name);
             this.id = devices.id;
           });
         this.connect(devices.id);
       }
     });
   }
-
+/* to connect  to the peripheral and get  his UUIDs
+we could define a json file where  we declare UUID's as constant*/
   connect(id_dev: string) {
+
     this.ble.connect(id_dev).subscribe(peripheralData => {
       this.Bt_service = peripheralData.characteristics[10].service;
       this.cadns_service = peripheralData.characteristics[13].service;
@@ -73,67 +70,104 @@ export class HomePage {
       this.cadns_char = peripheralData.characteristics[13].characteristic;
       this.rev_char = peripheralData.characteristics[12].characteristic;
       this.flashdt_char = peripheralData.characteristics[14].characteristic;
-      console.log("tall001"+ this.cadns_char);
       this.read_battery_level();
+      this.notify_battery_level();
     }, peripheralData => {
+     this.presentToast();
       console.log('disconnected:(');
     });
-    this.connection = true;
+  }
+  /*------------------------ Battery Setting---------------*/
+
+/*ngZone used to allow angular bind data in real time
+*/
+  read_battery_level() {
+    this.isValid = true;
+    this.ble.read(this.id, this.Bt_service, this.Bt_char)
+      .then(battery_level => {
+        console.log('reading now');
+        var level = this.onData(battery_level);
+        this.ngZone.run(() => { this.batteryLevel = level[0]; });
+        console.log('data is binding' + this.batteryLevel);
+        console.log('finish reading');
+      })
+      .catch(error => { console.log('failure' + error); });
   }
 
-
-  read_battery_level() {
+  notify_battery_level() {
+    console.log('subscribe to notify');
     this.ble.startNotification(this.id, this.Bt_service, this.Bt_char)
       .subscribe(notificationdata => {
         var dt = this.onData(notificationdata);
-        this.Bt_lev = dt[0];
-        alert('Battery level : ' + this.Bt_lev);
+        this.ngZone.run(() => { this.batteryLevel = dt[0]; });
+        console.log('notify :' + this.batteryLevel);
       });
   }
 
+
+  /*------------------------Cadence Setting---------------*/
+  get_cadence() {
+    this.navCtrl.push(CadencePage);
+  }
+  /*------------------------Revolution Number Setting---------------*/
+
   get_revolution_number() {
-    this.ble.read(this.id, this.rev_service, this.rev_char)
+
+    this.isRev =!this.isRev;
+      this.ble.read(this.id, this.rev_service, this.rev_char)
       .then(revolution_data => {
-        this.last_revolution_nb = new Uint32Array(revolution_data);
+        this.ngZone.run(() => { this.last_revolution_nb = new Uint32Array(revolution_data);});
         console.log(' last revolution nb :' + this.last_revolution_nb);
       });
 
-    console.log('start getting revonumber');
-    this.ble.startNotification(this.id, this.rev_service, this.rev_char)
+    console.log('Start Getting Revo Number');
+   this.RevoSubscription= this.ble.startNotification(this.id, this.rev_service, this.rev_char)
       .subscribe(revolution_data => {
         var revolution__data_view = new Uint32Array(revolution_data);
         var nb = (revolution__data_view[0] & 0xff) +
           ((revolution__data_view[1] & 0xff) << 8) +
           ((revolution__data_view[2] & 0xff) << 16) +
           ((revolution__data_view[3] & 0xff) << 24);
-        this.revolution_nb = nb - this.last_revolution_nb;
+        this.revolution_nb = nb - this.last_revolution_nb-1;
+        this.ngZone.run(() => { this.revolution_nb; });
         console.log('rev_c:' + this.revolution_nb);
       });
   }
 
-  get_cadence() {
-    this.navCtrl.push(CadencePage);
-    console.log('id****'+JSON.stringify(this.id));
-  }
 
-  get_chunk() {
-    console.log('start getting chunk');
-    this.ble.read(this.id, this.flashdt_service, this.flashdt_char)
-      .then(chunk_data => {
-        var chunk_data_view = this.onData(chunk_data);
-        var rslt = chunk_data_view[0] + ((chunk_data_view[1] & 0x0f) << 8);
-        var rt = chunk_data_view[5];
-        console.log('chunk_details' + rslt);
-        console.log('avercd:' + rt);
-      })
-      .catch(Error => { console.log('chinkerr' + Error); });
 
-  }
+  // get_chunk() {
+  //   console.log('start getting chunk');
+  //   this.ble.read(this.id, this.flashdt_service, this.flashdt_char)
+  //     .then(chunk_data => {
+  //       var chunk_data_view = this.onData(chunk_data);
+  //       var rslt = chunk_data_view[0] + ((chunk_data_view[1] & 0x0f) << 8);
+  //       var rt = chunk_data_view[5];
+  //       console.log('chunk_details' + rslt);
+  //       console.log('avercd:' + rt);
+  //     })
+  //     .catch(Error => { console.log('chinkerr' + Error); });
+
+  // }
 
   onData(buffer) {
+//Convert received data in array buffer
     var data = new Uint8Array(buffer);
     return data;
   }
 
-}
+  ionViewWillLeave(){
+     this.RevoSubscription.unsubscribe();
+  }
 
+/*Sometimes Sole disconnect automatically
+this function is called when disconnect from sole*/
+  presentToast() {
+   let toast = this.toastCtrl.create({
+    message: ' You are now disconnected ! Restart Application to reconnect',
+    duration: 10000,
+    position: 'top'
+  });
+  toast.present();
+}
+}
